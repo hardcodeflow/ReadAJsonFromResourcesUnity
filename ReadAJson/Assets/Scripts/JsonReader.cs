06c8021d3803585430f657511b2189dab5e7ed5a
Rok <hexgen.games@gmail.com>
2019-08-11 15:13:00 -0500

    enjoy

---
+++ ReadAJson/Assets/Scripts/JsonReader.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonReader : MonoBehaviour
+{
+    private List<LevelData> levels;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levels = new List<LevelData>();
+        TextAsset json = (TextAsset)Resources.Load("JsonData/levels_example");
+        try
+        {
+            levels = JsonConvert.DeserializeObject<List<LevelData>>(json.ToString());
+            foreach (LevelData level in levels) {
+                Debug.Log("id:" + level.id + "name: " + level.name + " description: " + level.description);
+            }
+        }
+        catch (Exception ex) {
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ReadAJson/Assets/Scripts/LevelData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelData 
+{
+    public int id;
+    public string name;
+    public string description;
+    public string sceneName;
+}
